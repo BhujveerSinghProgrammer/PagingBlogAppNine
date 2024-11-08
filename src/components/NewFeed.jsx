a0d1b0ad2d7e6b89a46d7415e912d465c7e885c1
USER <USER@DESKTOP-GAP4S8J>
2024-11-08 12:57:20 +0530

    fdffddfdf

--- src/components/NewFeed.jsx
+++ src/components/NewFeed.jsx
@@ -1,115 +1,83 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
-import { loadAllPosts,loadAllPostsByPageNumberandPageSize } from '../services/post-service';
-import { Row,Col,Pagination,PaginationItem,PaginationLink, Container } from 'reactstrap';
+import React, { useState, useEffect } from 'react';
+import { loadAllPostsByPageNumberandPageSize } from '../services/post-service';
+import { Row, Col, Pagination, PaginationItem, PaginationLink, Container } from 'reactstrap';
 import Posts from './Posts';
 import { toast } from 'react-toastify';
 
+function NewFeed() {
+  const [postContent, setPostContent] = useState({
+    Contents: [],
+    LastPage: 0,
+    PageNumber: 1,  // Start at page 1 (1-based)
+    PageSize: 5,   // Default page size
+    TotalElements: 0,
+    TotalPages: 0
+  });
 
-function NewFeed() 
-{
-const[postContent,setPostContent]=useState(
-   {
-      Contents:[],
-      LastPage:'',
-      PageNumber:'',
-      PageSize:'',
-      TotalElements:'',
-      TotalPages:''
-   }
- );
+  // Fetch paged posts when the page is first loaded
+  useEffect(() => {
+    loadAllPostsByPageNumberandPageSize(postContent.PageNumber, postContent.PageSize)
+      .then((data) => {
+        console.log('Initial Data', data);
+        setPostContent(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Error in Loading Posts');
+      });
+  }, []); // Only runs on component mount
 
-  useEffect(()=>{
-  //load all the posts from
-  loadAllPosts().then((data)=>{
-    console.log(data);
-    setPostContent(data);
-    console.log('details');
-    console.log(data.LastPage);
-    console.log(data.PageNumber);
+  // Handle page change
+  const changePage = (pageNumber) => {
+    if (pageNumber < 1 || pageNumber > postContent.TotalPages) return; // Do nothing if out of bounds
 
-  }).catch(error=>{
-    console.log(error);
-    toast.error("Error in Loading All Posts");
-  })
-},[]
-);
-
-const changePage=(PageNumberInput=0,PageSizeInput=5)=>
-{
-  loadAllPostsByPageNumberandPageSize(PageNumberInput,PageSizeInput).then(data=>{
- setPostContent(data);
-   console.log(PageNumberInput);
-    console.log(PageSizeInput);
-
-  }).catch(error=>{
-    toast.error("Error in Loading Posts Pagewise");
-  })
-}
-
-
+    loadAllPostsByPageNumberandPageSize(pageNumber, postContent.PageSize)
+      .then((data) => {
+        setPostContent(data);  // Update with new page data
+      })
+      .catch((error) => {
+        toast.error('Error in Loading Posts Pagewise');
+      });
+  };
 
   return (
+    <div className="container-fluid">
+      <Row>
+        <Col md={{ size: 10, offset: 1 }}>
+          <h1>Blogs Count ({postContent?.TotalElements})</h1>
+          {postContent?.Contents?.map((post) => (
+            <Posts post={post} key={post.Id} />
+          ))}
 
-   <div className="container-fluid">
-<Row>
-  <Col md={{
-    size:10,
-    offset:1
-  }} >
-    {/* {JSON.stringify(postContent)} */}
-    <h1>Blogs Count ({postContent?.TotalElements})</h1>
-  
-   
-  {
-    postContent?.Contents?.map((post)=>(
-      <Posts post={post} key={post.Id} />
-    ))
-  }
+          {/* Pagination component */}
+          <Container className="text-center mt-1">
+            <Pagination>
+              {/* Previous button */}
+              <PaginationItem disabled={postContent.PageNumber === 1}>
+                <PaginationLink previous onClick={() => changePage(postContent.PageNumber - 1)} />
+              </PaginationItem>
 
+              {/* Page number buttons */}
+              {[...Array(postContent.TotalPages)].map((_, index) => (
+                <PaginationItem
+                  key={index}
+                  onClick={() => changePage(index + 1)}  // Page numbers should be 1-based
+                  active={index + 1 === postContent.PageNumber}
+                >
+                  <PaginationLink>{index + 1}</PaginationLink>
+                </PaginationItem>
+              ))}
 
-{/* paging implementation */}
-<Container className='text-center mt-1'>
-  <Pagination>
-  
-  <PaginationItem disabled={postContent.PageNumber==0}>
-    <PaginationLink previous>
-
-    </PaginationLink>
-  </PaginationItem>
-
-
-
-{
-[...Array(postContent.TotalPages)].map((item,index)=>(
-  <PaginationItem onClick={()=>{changePage(index+1,3)}} active={index==postContent.PageNumber-1}  key={index} >
-    <PaginationLink >
-    {index+1}
-    </PaginationLink>
-  </PaginationItem>
-  ))
+              {/* Next button */}
+              <PaginationItem disabled={postContent.PageNumber === postContent.TotalPages}>
+                <PaginationLink next onClick={() => changePage(postContent.PageNumber + 1)} />
+              </PaginationItem>
+            </Pagination>
+          </Container>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-
-<PaginationItem disabled={postContent.LastPage==postContent.PageNumber}>
-    <PaginationLink next>
-
-    </PaginationLink>
-  </PaginationItem>
-
-
-
-
-</Pagination>
-</Container>
-
-
-  </Col>
-</Row>
-   </div>
-
-   
-  )
-}
-
-export default NewFeed
+export default NewFeed;
