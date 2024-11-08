aac61765f32db7c4bea71c3a979cfb5b85e7113e
USER <USER@DESKTOP-GAP4S8J>
2024-11-08 14:47:12 +0530

    ggg

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -6,11 +6,11 @@ function Posts({ post = { Title: "This is Default Post Title", Content: "This is
     <Card className='border-0 shadow-sm mb-3'>
       <CardBody>
         <h1>{post.Title}</h1>
-
-        {/* No need for <p> tag here, CardText already renders a <p> */}
         {/* <CardText>{post.Content}</CardText> */}
-{/* if we want to display only 10 charcaters */}
-  <CardText>{post.Content.substring(0,30)}...</CardText>
+{/* if we want to display only 30 charcaters */}
+  <CardText dangerouslySetInnerHTML={{ __html:post.Content.substring(0,60)+"..."}}>
+    {/* {post.Content.substring(0,30)}... */}
+    </CardText>
         <div>
           <Button>Read More..</Button>
         </div>
